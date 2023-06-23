7c8922d08325eace824e9a93b65a0bbffada623e
kntstphn <kentstephensumalinog@gmail.com>
2023-06-23 14:09:54 +0800

    Implement Rectangle Section

--- lib/components/blocks/footerNav/nav.tsx
+++ lib/components/blocks/footerNav/nav.tsx
@@ -8,7 +8,7 @@ export default function Nav() {
       <div className="flex items-center ml-10">
         <Image src={SouthGate} alt="SouthGate" width={300} height={300} />
       </div>
-      <nav className="flex w-full justify-end items-center text-black text-carrois text-2xl font-bold">
+      <nav className="flex w-full justify-end items-center gap-8 text-white px-10 mx-10 text-carrois text-2xl">
         <a href="#" className=" px-4 hover:text-gray-300">
           HOME
         </a>
