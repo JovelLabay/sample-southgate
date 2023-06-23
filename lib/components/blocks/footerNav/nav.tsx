800f2fed70f9ee5e3147fb23121258d73bbad8cf
kntstphn <kentstephensumalinog@gmail.com>
2023-06-23 09:48:37 +0800

    Update FooterNav: Adjust Font sizes

--- lib/components/blocks/footerNav/nav.tsx
+++ lib/components/blocks/footerNav/nav.tsx
@@ -5,10 +5,10 @@ import SouthGate from "public/images/southgate.png";
 export default function Nav() {
   return (
     <div className="flex py-2 bg-transparent">
-      <div className="flex items-center border-black border-solid">
-        <Image src={SouthGate} alt="SouthGate" width={500} height={500} />
+      <div className="flex items-center ml-10">
+        <Image src={SouthGate} alt="SouthGate" width={300} height={300} />
       </div>
-      <nav className="flex w-full justify-end items-center text-black text-carrois text-3xl font-bold">
+      <nav className="flex w-full justify-end items-center text-black text-carrois text-2xl font-bold">
         <a href="#" className=" px-4 hover:text-gray-300">
           HOME
         </a>
