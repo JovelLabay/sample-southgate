7cd6714e5d4cdeddd72ebe2333dbd95818f463be
kntstphn <kentstephensumalinog@gmail.com>
2023-06-23 11:43:24 +0800

    Implement Banner Component

--- lib/components/blocks/home/banner.tsx
+++ lib/components/blocks/home/banner.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Image from "next/image";
 import Giorgio from "public/giorgio.png";
+import Logo from "public/images/logo.png";
 
 export default function HomeBanner() {
   return (
-    <div>
+    <div className="flex items-center">
       <Image src={Giorgio} alt="Giorgio" className="banner" />
-      <span className="text-white">Test</span>
     </div>
   );
 }
