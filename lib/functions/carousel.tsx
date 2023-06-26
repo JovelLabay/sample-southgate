b22dffcdccd143f705808259c4ca3976cfefd8a2
kntstphn <kentstephensumalinog@gmail.com>
2023-06-26 10:29:41 +0800

    Create Carousel file

---
+++ lib/functions/carousel.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  name: string;
+  statement: string;
+  title: string;
+  image: string;
+  company: string;
+};
+
+export default function CarouselStatements({
+  name,
+  statement,
+  title,
+  image,
+  company,
+}: Props) {
+  return <div>Statement</div>;
+}
