946040976b541d4264f6ce47bbe794af48119f23
kntstphn <kentstephensumalinog@gmail.com>
2023-06-27 14:11:49 +0800

    Created the middle section for Aboutpage: Parallelogram section containing two images and a video

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,36 +5,8 @@ import Banner from "@/lib/components/blocks/aboutPage/banner";
 // import VideoJS from "@/lib/functions/videoJS";
 import Accordion from "@/lib/components/blocks/aboutPage/accordion/accordion";
 import Tyson from "@/public/images/tyson.png";
-// import SouthgateVideo from "../../public/videos/southgate.mp4";
 
 export default async function AboutPage() {
-  // const playerRef = React.useRef(null);
-
-  // const videoJsOptions = {
-  //   autoplay: true,
-  //   controls: true,
-  //   responsive: true,
-  //   fluid: true,
-  //   sources: [
-  //     {
-  //       src: "/path/to/video.mp4",
-  //       type: "video/mp4",
-  //     },
-  //   ],
-  // };
-
-  // const handlePlayerReady = (player) => {
-  //   playerRef.current = player;
-
-  //   // You can handle player events here, for example:
-  //   player.on("waiting", () => {
-  //     videojs.log("player is waiting");
-  //   });
-
-  //   player.on("dispose", () => {
-  //     videojs.log("player will dispose");
-  //   });
-  // };
   return (
     <main>
       <section>
@@ -42,49 +14,54 @@ export default async function AboutPage() {
       </section>
 
       <section className="flex flex-col mt-[90px] text-white">
-        {/* First Take */}
+        {/* First Portion */}
         <div className="parallelogram-background ">
-          <div className="skew-x-[8deg] flex margin items-center w-full h-full my-[70px]">
-            <div className="juliana">
-              <div className="juliana-overlay"></div>
-              <Image src={Tyson} alt="Tyson Photo" />
-            </div>
-            <div className=" flex flex-col w">
-              <div className="ml-10 w-[600px]">
-                <span className="text-demonicyellow text-goodpro text-[80px] font-extrabold">
-                  WHY CHOOSE US
-                </span>
-                <br />
-                <span className="text-carrois text-3xl">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam.
-                  </p>
-                </span>
+          <div className="skew-x-[8deg] flex margin w-full h-full my-[70px] flex-col gap-[100px]">
+            <div className="flex">
+              <div className="juliana">
+                <div className="juliana-overlay"></div>
+                <Image src={Tyson} alt="Tyson Photo" />
+              </div>
+              <div className=" flex flex-col w">
+                <div className="ml-10 w-[600px]">
+                  <span className="text-demonicyellow text-goodpro text-[80px] font-extrabold">
+                    WHY CHOOSE US
+                  </span>
+                  <br />
+                  <span className="text-carrois text-3xl">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam.
+                    </p>
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* <div>
-            <h2>Video Player</h2>
-            <video src={SouthgateVideo} width="800" height="400" controls />
-          </div> */}
-
-          {/* Second Take */}
-          <div className="skew-x-[8deg] flex margin items-center w-full h-full my-[70px]">
-            <div className="juliana">
-              <div className="juliana-overlay"></div>
-              <Image src={Tyson} alt="Tyson Photo" />
+            {/* Second Portion (video) */}
+            <div className="flex">
+              <video
+                src="/videos/southgate.mp4"
+                width="80%"
+                height="400"
+                controls
+              />
             </div>
 
-            <div className=" flex flex-col w">
+            {/* Third portion */}
+            <div className="flex">
+              <div className="juliana">
+                <div className="juliana-overlay"></div>
+                <Image src={Tyson} alt="Tyson Photo" />
+              </div>
+
               <div className="ml-10 w-[600px]">
                 <span className="text-demonicyellow text-goodpro text-[80px] font-extrabold">
                   HOW WE STARTED
