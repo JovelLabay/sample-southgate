566379a44c15d52b8f42bc500f029b2206f47267
kntstphn <kentstephensumalinog@gmail.com>
2023-06-30 09:15:08 +0800

    Add Maps

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Banner from "@/lib/components/blocks/contactPage/banner";
 import ContactForm from "@/lib/components/blocks/contactPage/contactForm";
+import MapView from "@/lib/components/blocks/contactPage/mapView";
 
 export default async function ContactPage() {
   return (
@@ -13,6 +14,17 @@ export default async function ContactPage() {
       {/* Contact Forms */}
       <section>
         <ContactForm />
+      </section>
+      {/* Google Maps */}
+      <section>
+        <div className="w-full flex flex-row items-center justify-left h-auto margin-y -ml-[100px]">
+          <div className="flex items-center justify-end -rotate-90 w-[700px] -ml-[100px]">
+            <span className="text-goodpro-bold text-[60px] transform -translate-x-2xl">
+              FIND US
+            </span>
+          </div>
+          <MapView />
+        </div>
       </section>
     </main>
   );
