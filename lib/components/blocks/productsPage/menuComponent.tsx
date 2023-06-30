4bd5f86836796a91563da34eb5db404b1e508ba9
kntstphn <kentstephensumalinog@gmail.com>
2023-06-30 16:34:07 +0800

    Update Products Component: Separate to components and add working Pagination

--- lib/components/blocks/productsPage/menuComponent.tsx
+++ lib/components/blocks/productsPage/menuComponent.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import MenuCategories from "./menuCategories";
 import Laursoo from "@/public/images/laursoo.png";
 import Button from "@/lib/functions/button";
 import { SlArrowRight, SlArrowLeft } from "react-icons/sl";
+import ProductList from "./productList";
+import Pagination from "./pagination";
 
 type Props = {
   menuData: {
@@ -22,45 +25,57 @@ type MenuItem = {
   image: string;
 };
 
+const getSortedItems = (
+  category: string,
+  currentPage: number,
+  itemsPerPage: number,
+  menuData: Props["menuData"]
+) => {
+  const items: MenuItem[] = [];
+
+  if (category === "ALL") {
+    Object.values(menuData).forEach((categoryItems) => {
+      items.push(...categoryItems);
+    });
+  } else if (category !== "ALL" && category !== "") {
+    items.push(...menuData[category]);
+  }
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const sortedItems = items.sort((a, b) => a.name.localeCompare(b.name));
+  const slicedItems = items.slice(startIndex, endIndex);
+
+  return slicedItems;
+};
+
 export default function MenuComponent({ menuData }: Props) {
-  const [activeCategory, setActiveCategory] = useState<string | null>(null);
+  const [activeCategory, setActiveCategory] = useState("ALL");
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const [allItems, setAllItems] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 3;
 
-  let allItems = 0;
-
   const handleClick = (category: string) => {
-    setActiveCategory(category === activeCategory ? null : category);
+    setActiveCategory(category === activeCategory ? "" : category);
     setSelectedItem(null);
     setCurrentPage(1); // Reset currentPage to 1
   };
 
-  const handleItemClick = (itemName: string) => {
-    setSelectedItem(itemName === selectedItem ? null : itemName);
-  };
+  useEffect(() => {
+    const items: MenuItem[] = [];
 
-  // Sort items for different categories
-  const getSortedItems = (category: string, page: number) => {
-    const items = menuData[category] || [];
-    const startIndex = (page - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    allItems = items.length;
-    return items
-      .slice(startIndex, endIndex)
-      .sort((a, b) => a.name.localeCompare(b.name));
-  };
+    if (activeCategory === "ALL") {
+      Object.values(menuData).forEach((categoryItems) => {
+        items.push(...categoryItems);
+      });
+    } else if (activeCategory !== "ALL" && activeCategory !== "") {
+      items.push(...menuData[activeCategory]);
+    }
 
-  // Sort all items by combining all category details and sorting it
-  const getAllItems = () => {
-    const allItems: MenuItem[] = [];
-    Object.values(menuData).forEach((categoryItems) => {
-      allItems.push(...categoryItems);
-    });
-    return allItems.sort((a, b) => a.name.localeCompare(b.name));
-  };
+    setAllItems(items.length); // Update the total number of items
+  }, [activeCategory, menuData]);
 
-  // Handle pagination navigation
   const goToPreviousPage = () => {
     setCurrentPage((prevPage) => prevPage - 1);
   };
@@ -75,135 +90,29 @@ export default function MenuComponent({ menuData }: Props) {
 
   return (
     <div>
-      <div className="menu-parallelogram flex mt-[120px]">
-        <div className="flex skew-x-[20deg] text-goodpro gap-[90px] margin justify-center items-center text-white">
-          <h2
-            className={`font-bold text-5xl cursor-pointer ${
-              activeCategory === "ALL" || activeCategory === null
-                ? "text-yellow-500 underline underline-offset-4"
-                : ""
-            }`}
-            onClick={() => handleClick("ALL")}
-          >
-            ALL
-          </h2>
-          {Object.keys(menuData).map((category, index) => (
-            <div key={index} className="cursor-pointer">
-              <h2
-                className={`font-bold text-5xl ${
-                  activeCategory === category
-                    ? "text-yellow-500  underline underline-offset-4"
-                    : ""
-                }`}
-                onClick={() => handleClick(category)}
-              >
-                {category.toUpperCase()}
-              </h2>
-            </div>
-          ))}
-        </div>
-      </div>
+      {/* Menu Categories */}
+      <MenuCategories
+        activeCategory={activeCategory}
+        handleClick={handleClick}
+        menuData={menuData}
+      />
       {/* Details */}
-      <div className="flex flex-col w-auto h-auto bg-green-300 margin">
-        {activeCategory === "ALL" || activeCategory === null ? (
-          <div className="mt-5">
-            {getAllItems().map((item) => (
-              <div
-                key={item.name}
-                className={`flex ${
-                  selectedItem === item.name ? "selected" : ""
-                }`}
-                onClick={() => handleItemClick(item.name)}
-              >
-                <div className="flex flex-row my-5">
-                  <Image
-                    src={Laursoo}
-                    alt="Laursoo"
-                    width={1000}
-                    height={700}
-                  />
-                  <div className="flex flex-col ml-10 justify-center items-left w-auto">
-                    <h3 className="text-goodpro font-extrabold text-[50px]">
-                      {item.name}
-                    </h3>
-                    <span className="text-goodpro -mt-2 text-[25px]">
-                      {item.label.toUpperCase()}
-                    </span>
-                    <div className="w-[85%]">
-                      <p className="text-carrois mt-3 text-[34px]">
-                        {item.description}
-                      </p>
-                    </div>
-                    <div className="-ml-10">
-                      <Button label="DOWNLOAD BROCHURE" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div>
-            {getSortedItems(activeCategory || "", currentPage).map((item) => (
-              <div
-                key={item.name}
-                className={`item ${
-                  selectedItem === item.name ? "selected" : ""
-                }`}
-                onClick={() => handleItemClick(item.name)}
-              >
-                <div className="flex flex-row my-5">
-                  <Image
-                    src={Laursoo}
-                    alt="Laursoo"
-                    width={1000}
-                    height={700}
-                  />
-                  <div className="flex flex-col ml-10 justify-center items-left w-auto">
-                    <h3 className="text-goodpro font-extrabold text-[50px]">
-                      {item.name}
-                    </h3>
-                    <span className="text-goodpro -mt-2 text-[25px]">
-                      {item.label.toUpperCase()}
-                    </span>
-                    <div className="w-[85%]">
-                      <p className="text-carrois mt-3 text-[34px]">
-                        {item.description}
-                      </p>
-                    </div>
-                    <div className="-ml-10">
-                      <Button label="DOWNLOAD BROCHURE" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-      <div className="flex justify-center items-center gap-20 my-3">
-        {currentPage > 1 && (
-          <button onClick={goToPreviousPage}>
-            <div className="text-3xl">
-              <SlArrowLeft />
-            </div>
-          </button>
-        )}
-        {activeCategory === "ALL" || activeCategory === null ? (
-          <span className="text-3xl">{currentPage}</span>
-        ) : (
-          <>
-            <span>{currentPage}</span>
-          </>
-        )}
-        {getAllItems().length > itemsPerPage && (
-          <button onClick={goToNextPage}>
-            <div className="text-3xl">
-              <SlArrowRight />
-            </div>
-          </button>
-        )}
-      </div>
+      <ProductList
+        getSortedItems={() =>
+          getSortedItems(activeCategory, currentPage, itemsPerPage, menuData)
+        }
+        activeCategory={activeCategory}
+        selectedItem={selectedItem}
+      />
+      {/* Pagination */}
+      <Pagination
+        allItems={allItems}
+        currentPage={currentPage}
+        goToPreviousPage={goToPreviousPage}
+        goToNextPage={goToNextPage}
+        activeCategory={activeCategory}
+        itemsPerPage={itemsPerPage}
+      />
     </div>
   );
 }
