10fe6fba8421dcfd32cb7a26854896a679a4496e
kntstphn <kentstephensumalinog@gmail.com>
2023-07-03 10:38:13 +0800

    Fix Scaling: Fix scaling for products page

--- lib/components/blocks/productsPage/menuCategories.tsx
+++ lib/components/blocks/productsPage/menuCategories.tsx
@@ -17,10 +17,10 @@ const MenuCategories: React.FC<MenuCategoriesProps> = ({
   menuData,
 }) => {
   return (
-    <div className="menu-parallelogram flex mt-[120px]">
+    <div className="menu-parallelogram flex mt-[70px]">
       <div className="flex skew-x-[20deg] text-goodpro gap-[90px] margin justify-center items-center text-white">
         <h2
-          className={`font-bold text-5xl cursor-pointer ${
+          className={`font-bold text-3xl cursor-pointer ${
             activeCategory === "ALL" || activeCategory === null
               ? "text-yellow-500 underline underline-offset-4"
               : ""
@@ -32,7 +32,7 @@ const MenuCategories: React.FC<MenuCategoriesProps> = ({
         {Object.keys(menuData).map((category, index) => (
           <div key={index} className="cursor-pointer">
             <h2
-              className={`font-bold text-5xl ${
+              className={`font-bold text-3xl ${
                 activeCategory === category
                   ? "text-yellow-500  underline underline-offset-4"
                   : ""
--- lib/components/blocks/productsPage/productList.tsx
+++ lib/components/blocks/productsPage/productList.tsx
@@ -27,30 +27,35 @@ const ProductList: React.FC<ProductListProps> = ({
       <div>
         {/* Other Categories */}
         {getSortedItems(activeCategory || "").map((item) => (
-          <div
-            key={item.name}
-            className={`item ${selectedItem === item.name ? "selected" : ""}`}
-          >
-            <div className="flex flex-row my-5">
-              <Image src={Laursoo} alt="Laursoo" width={1000} height={700} />
-              <div className="flex flex-col ml-10 justify-center items-left w-auto">
-                <h3 className="text-goodpro font-extrabold text-[50px]">
-                  {item.name}
-                </h3>
-                <span className="text-goodpro -mt-2 text-[25px]">
-                  {item.label.toUpperCase()}
-                </span>
-                <div className="w-[85%]">
-                  <p className="text-carrois mt-3 text-[34px]">
-                    {item.description}
-                  </p>
-                </div>
-                <div className="-ml-10">
-                  <Button label="DOWNLOAD BROCHURE" />
+          <>
+            <div
+              key={item.name}
+              className={`item py-[50px] ${
+                selectedItem === item.name ? "selected" : ""
+              }`}
+            >
+              <div className="flex flex-row my-5">
+                <Image src={Laursoo} alt="Laursoo" width={700} height={700} />
+                <div className="flex flex-col ml-10 justify-center items-left w-auto">
+                  <h3 className="text-goodpro font-extrabold text-[40px]">
+                    {item.name}
+                  </h3>
+                  <span className="text-goodpro -mt-2 text-[18px]">
+                    {item.label.toUpperCase()}
+                  </span>
+                  <div className="w-[85%]">
+                    <p className="text-carrois mt-3 text-[25px]">
+                      {item.description}
+                    </p>
+                  </div>
+                  <div className="-ml-10">
+                    <Button label="DOWNLOAD BROCHURE" />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+            <hr className="full-width-hr" />
+          </>
         ))}
       </div>
     </div>
