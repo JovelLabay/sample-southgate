9543cc78a443db276af8114f0c6e696dd3e041b1
kntstphn <kentstephensumalinog@gmail.com>
2023-07-03 13:27:34 +0800

    Added Client details in about us

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -29,15 +29,21 @@ export default async function AboutPage() {
                   <br />
                   <span className="text-carrois text-2xl">
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      In 2017, Southgate formally launched its direct
+                      importation and distributorship of the Italian Brand
+                      “Pirelli” which is known for its affiliation and exclusive
+                      supply of tires to the Formula 1 racing circuit. The
+                      company carries a very extensive line of Pirelli Light
+                      Truck Radial (LTR) and Truck Bus Radial (TBR) sizes.
                     </p>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam.
+                      In 2021, Prometeon Tyre Group awarded exclusive
+                      Distributorship for its TBR and LTR business.
+                    </p>
+                    <p>
+                      In 2022, the company added the Zeetex 5000max PCR range
+                      into its product portfolio. It is manufactured in Thailand
+                      by ZAFCO International with headquarters in Dubai, UAE.
                     </p>
                   </span>
                 </div>
@@ -69,15 +75,13 @@ export default async function AboutPage() {
                   <br />
                   <span className="text-carrois text-2xl">
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam.
+                      Together with its dynamic marketing approach and strong
+                      AFTERSALES support, Southgate has built a very formidable
+                      portfolio catering across all market segments. With the
+                      country’s increasing demand for Quality yet Affordable
+                      tires, our Pirelli, Aeolus and Zeetex product lines can
+                      surely meet the expectations of every consumer’s
+                      requirements.
                     </p>
                   </span>
                 </div>
