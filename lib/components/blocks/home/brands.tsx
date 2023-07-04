8560ed1077beaf310379ee91751e89422eec3a45
kntstphn <kentstephensumalinog@gmail.com>
2023-07-04 14:33:52 +0800

    Finish mobile opt for brand list in homepage

--- lib/components/blocks/home/brands.tsx
+++ lib/components/blocks/home/brands.tsx
@@ -12,16 +12,19 @@ type Brand = {
 
 export default function DynamicBrands({ brands }: Props) {
   return (
-    <div className="flex flex-row margin-y justify-end  h-[500px]">
-      <div className="-rotate-90 flex  h-[200px] w-[650px] items-start justify-center -mr-10 mt-[160px]">
-        <span className="flex text-goodpro-bold text-[60px] w-[500px] mt-10 items-end align-bottom justify-center">
+    <div className="flex flex-col sm:flex-row margin-y justify-center  sm:h-[500px]">
+      <div className="sm:-rotate-90 flex  h-auto w-full sm:h-[200px] sm:w-[650px] items-center justify-center sm:-mr-10 sm:mt-[160px]">
+        <span className="flex text-goodpro-bold text-[40px] sm:text-[60px] w-[500px] sm:mt-10 items-center align-center justify-center sm:justify-end">
           OUR BRANDS
         </span>
       </div>
-      <div className="flex flex-wrap justify-start align-middle items-center gap-[70px] w-[86%] h-auto">
+      <div className="flex flex-wrap justify-center align-middle items-center gap-[50px] sm:gap-[70px] w-full sm:w-[86%] h-auto">
         {brands.map((brand, index) => (
-          <div key={index} className="w-[337px] flex h-[89px]">
-            <Image src={Pirelli} alt={brand.name} width={337} height={9} />
+          <div
+            key={index}
+            className="sm:w-[350px] w-[150px] h-fit flex sm:h-[100px] justify-center m-3"
+          >
+            <Image src={Pirelli} alt={brand.name} />
           </div>
         ))}
       </div>
