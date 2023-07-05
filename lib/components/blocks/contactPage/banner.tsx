440f4529963f4784c883b07b8fc36da1b96b6866
kntstphn <kentstephensumalinog@gmail.com>
2023-07-05 14:51:21 +0800

    Mobile Optimization in contacts page for banner section

--- lib/components/blocks/contactPage/banner.tsx
+++ lib/components/blocks/contactPage/banner.tsx
@@ -6,18 +6,13 @@ export default function Banner() {
   return (
     <div className="margin">
       <div className="flex items-center ">
-        <Image
-          src={Nilsson}
-          alt="Nilsson"
-          className="banner"
-          style={{ height: "410px" }}
-        />
+        <Image src={Nilsson} alt="Nilsson" className="banner-contacts" />
       </div>
-      <div className="flex flex-col items-start mt-[100px]">
-        <span className="text-carrois text-demonicyellow text-3xl font-light">
+      <div className="flex flex-col items-start mt-[80px] sm:mt-[100px]">
+        <span className="text-carrois text-demonicyellow sm:text-1xl sm:text-3xl font-light">
           About Us
         </span>
-        <span className="text-goodpro-bold text-white text-5xl mt-3">
+        <span className="text-goodpro-bold text-white text-3xl sm:text-5xl mt-3">
           GET TO KNOW THE COMPANY
         </span>
       </div>
