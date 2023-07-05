f398f1629edc05b81aafdf4179716d4001853e9a
kntstphn <kentstephensumalinog@gmail.com>
2023-07-05 10:41:17 +0800

    Mobile Optimize products page banner

--- lib/components/blocks/productsPage/banner.tsx
+++ lib/components/blocks/productsPage/banner.tsx
@@ -6,18 +6,13 @@ export default function Banner() {
   return (
     <div className="margin">
       <div className="flex items-center ">
-        <Image
-          src={Pinto}
-          alt="Pinto"
-          className="banner"
-          style={{ height: "380px" }}
-        />
+        <Image src={Pinto} alt="Pinto" className="banner-products" />
       </div>
-      <div className="flex flex-col items-start mt-[100px]">
-        <span className="text-carrois text-demonicyellow text-3xl font-light">
+      <div className="flex flex-col items-start mt-[80px] sm:mt-[100px]">
+        <span className="text-carrois text-demonicyellow sm:text-1xl sm:text-3xl font-light">
           OUR PRODUCTS
         </span>
-        <span className="text-goodpro-bold text-white text-5xl mt-3">
+        <span className="text-goodpro-bold text-white text-3xl sm:text-5xl mt-3">
           SEE OUR LATEST TYRES
         </span>
       </div>
