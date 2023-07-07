6706b2980e0670d0e66efbad0c1ccb27a4ddab30
kentsuma <kentsumalinog.work@gmail.com>
2023-07-07 19:48:34 +0800

    Adjust home banner text to align

--- lib/components/blocks/home/homeBanner.tsx
+++ lib/components/blocks/home/homeBanner.tsx
@@ -5,26 +5,30 @@ import Logo from "public/images/logo.png";
 
 export default function HomeBanner() {
   return (
-    <div className="margin">
-      <div className="flex items-center">
+    <>
+      <div className="items-center">
         <Image src={Giorgio} alt="Giorgio" className="banner" />
       </div>
-      <div className="py-8 justify-start mt-[60px]">
-        <Image
-          src={Logo}
-          alt="Southgate Express marketing Inc."
-          width={130}
-          height={70}
-        />
+
+      <div className="margin">
+        <div className="flex flex-none mt-3 sm:flex-col">
+          <div className="flex py-8 sm:mt-[60px] w-auto h-full">
+            <Image
+              src={Logo}
+              alt="Southgate Express marketing Inc."
+              className="sm:w-[130px] sm:h[70px] w-[90px] h-auto"
+            />
+          </div>
+          <div className="flex flex-col justify-center sm:items-start ml-4 sm:ml-0">
+            <span className="text-carrois text-demonicyellow sm:text-1xl sm:text-3xl font-light">
+              Lorem Ipsum
+            </span>
+            <span className="text-goodpro-bold text-white text-2xl sm:text-5xl mt-3">
+              TECHNOLOGY MEETS <br /> PERFORMANCE
+            </span>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col items-start ">
-        <span className="text-carrois text-demonicyellow sm:text-1xl sm:text-3xl font-light">
-          Lorem Ipsum
-        </span>
-        <span className="text-goodpro-bold text-white text-3xl sm:text-5xl mt-3">
-          TECHNOLOGY MEETS <br /> PERFORMANCE
-        </span>
-      </div>
-    </div>
+    </>
   );
 }
