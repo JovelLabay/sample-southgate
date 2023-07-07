b0534434053c4e7a4d6ab627e5aeecf11ea53f4d
kentsuma <kentsumalinog.work@gmail.com>
2023-07-07 16:59:44 +0800

    Fix FAQ in About Page

--- lib/components/blocks/aboutPage/accordion/accordion.tsx
+++ lib/components/blocks/aboutPage/accordion/accordion.tsx
@@ -21,9 +21,11 @@ export default function Accordion() {
   ];
 
   return (
-    <div className="flex justify-center w-full items-center bg-red-500 flex-col lg:flex-row">
-      <div className="lg:-rotate-90 sideways-text  justify-end md:mt-10 lg:mt-20 items-center lg:h-[500px] flex bg-blue-400 lg:ml-3 lg:-mr-5">
-        <h1 className="text-goodpro-bold text-[60px]">{"FAQ's"}</h1>
+    <div className="flex justify-center w-full items-center flex-col lg:flex-row">
+      <div className="lg:-rotate-90 sideways-text  sm:justify-end md:mt-10 lg:mt-20 items-center lg:h-[500px] flex lg:ml-3 lg:-mr-5 w-full sm:w-auto justify-center">
+        <h1 className="text-goodpro-bold text-[50px] sm:text-[60px] pt-3">
+          {"FAQ's"}
+        </h1>
       </div>
       <div className="flex flex-col margin justify-center items-center margin-y gap-5 w-[70%]">
         {accordionData.map((data, index) => (
