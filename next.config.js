325594abd33dd0f6cae005babc43afed7e03c5ae
kentsuma <kentsumalinog.work@gmail.com>
2023-07-25 16:02:27 +0800

    Change next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,10 @@ const nextConfig = {
   images: {
     domains: [process.env.WP_IMAGES_URL],
   },
-};
-
-module.exports = {
-  ...nextConfig,
   env: {
     NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN:
       process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
   },
 };
+
+module.exports = nextConfig;
